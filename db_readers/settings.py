922469b68d129216f490c313102c23bee9c6e001
Loosper <boian4o1@gmail.com>
2018-03-16 18:21:02 +0200

    fml

--- db_readers/settings.py
+++ db_readers/settings.py
@@ -1,6 +1,8 @@
 from os.path import abspath, dirname
 
 ROOT = abspath(dirname(dirname(__file__)))
+# DB_TYPE = 'mysql+mysqldb'
+# DB_PATH = 'c51segmentatio:mHf8hAkxVGH9@web01.hostbalkan.com/c51segmentatio'
 DB_TYPE = "sqlite+pysqlite"
 DB_PATH = '/' + ROOT + '/database.db'
 PORT = 8080
