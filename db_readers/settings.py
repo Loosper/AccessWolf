9a1138503bf8002da82ad262ba5425ce9c5b482e
Loosper <boian4o1@gmail.com>
2018-03-16 18:17:15 +0200

    Fixed

--- db_readers/settings.py
+++ db_readers/settings.py
@@ -1,5 +1,7 @@
 from os.path import abspath, dirname
 
 ROOT = abspath(dirname(dirname(__file__)))
+DB_TYPE = "sqlite+pysqlite"
+DB_PATH = '/' + ROOT + '/database.db'
 PORT = 8080
 HOST = 'localhost'
