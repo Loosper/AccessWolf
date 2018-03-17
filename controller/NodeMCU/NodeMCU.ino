896f5038a84e39dfcf788fd4ea72865a7ca254a9
root <vitonaychev@gmail.com>
2018-03-17 19:16:02 +0200

    Add WiFi and server configuration for ESP

--- controller/NodeMCU/NodeMCU.ino
+++ controller/NodeMCU/NodeMCU.ino
@@ -4,56 +4,119 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
+#include <FS.h>
+
 #include <ArduinoJson.h>
 
 #define SS_PIN D8
 #define RST_PIN D2
 
-const char* ssid = "SmartSwitch-801";
-const char* password = "abcd12348";
+String server_ip;
+int room;
 
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+const int TRIGGER_PIN2 = D3; //
+const char* CONFIG_FILE = "/config.json";
+
+bool initialConfig = false;
+bool readConfigFile();
+bool writeConfigFile();
+
+
+MFRC522 rfid(SS_PIN, RST_PIN); 
 
 MFRC522::MIFARE_Key key; 
 
-// Init array that will store new NUID 
 byte nuidPICC[4];
  
 void setup () {
+  SPIFFS.begin();
  
   Serial.begin(9600);
-  WiFi.begin(ssid, password);
    
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
 
+  pinMode(TRIGGER_PIN2, INPUT_PULLUP);
+  
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
- 
-  while (WiFi.status() != WL_CONNECTED) {
- 
-    delay(1000);
-    Serial.print("Connecting..");
- 
+
+  if (!readConfigFile()) {
+    Serial.println("Failed to read configuration file, using default values");
   }
+
+  WiFi.printDiag(Serial);
+
+  if (WiFi.SSID() == "") {
+    Serial.println("We haven't got any access point credentials, so get them now");
+    initialConfig = true;
+  } else {
+    
+    WiFi.mode(WIFI_STA); 
+    unsigned long startedAt = millis();
+    Serial.print("After waiting ");
+    int connRes = WiFi.waitForConnectResult();
+    float waited = (millis()- startedAt);
+    Serial.print(waited/1000);
+    Serial.print(" secs in setup() connection result is ");
+    Serial.println(connRes);
+  }
+
+  if (WiFi.status()!=WL_CONNECTED){
+    Serial.println("Failed to connect, finishing setup anyway");
+  } else{
+    Serial.print("Local ip: ");
+    Serial.println(WiFi.localIP());
+  }
+ 
 }
  
 void loop() {
- 
-  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+ if ( (digitalRead(TRIGGER_PIN2) == LOW) || (initialConfig)) {
+     Serial.println("Configuration portal requested");
+    
+    char convertedValue[4];
+    WiFiManagerParameter p_room("room", "Room Number", "", 4);
+    
+    WiFiManagerParameter p_server_ip("server_ip", "Server IP", "", 40);
+
+    WiFiManager wifiManager;
+    
+    wifiManager.addParameter(&p_room);
+    
+    wifiManager.addParameter(&p_server_ip);
+
+    if (!wifiManager.startConfigPortal()) {
+      Serial.println("Not connected to WiFi but continuing anyway.");
+    } else {
+     
+      Serial.println("Connected...yeey :)");
+    }
+
+    room = atoi(p_room.getValue());
+    server_ip = String(p_server_ip.getValue());
+    
+    writeConfigFile();
+
+    ESP.reset();
+    
+    delay(5000);
+  }else if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
     
     if ( ! rfid.PICC_IsNewCardPresent())
     return;
-
-    // Verify if the NUID has been readed
+    
     if ( ! rfid.PICC_ReadCardSerial())
       return;
       
     Serial.print(F("PICC type: "));
     MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
     Serial.println(rfid.PICC_GetTypeName(piccType));
-
+    
     // Check is the PICC of Classic MIFARE type
     if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
       piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
@@ -81,43 +144,37 @@ void loop() {
     JsonObject& root = jsonBuffer.createObject();
 
     root["uid"] = uid;
+    root["room"] = room;
+
     char jsonBody[100];
     root.printTo(jsonBody, root.measureLength() + 1);
     Serial.println(jsonBody);
     
-    HTTPClient http;  //Declare an object of class HTTPClient
+    HTTPClient http;
  
-    http.begin("http://192.168.43.142:3000/logs");  //Specify request destination
+    http.begin(server_ip);
     http.addHeader("Content-Type", "application/json");
-
-    int httpCode = http.POST(String(jsonBody));                //Send the request
- 
+    
+    int httpCode = http.POST(String(jsonBody));
+    Serial.print("HTTP request send to: ");
+    Serial.println(server_ip);
     if (httpCode == 200) { //Check the returning code
 
-        Serial.println("JSON Parsed succccsessfully");
-//      String payload = http.getString();   //Get the request response payload
-//      payload += '\n';
-//      Serial.print(payload);                     //Print the response payload
-// 
+    Serial.println("JSON Parsed succccsessfully");
+
     } else if(httpCode == 401){
       Serial.println("No such database entry");
     }
  
     http.end();   //Close connection
- 
-  }
- 
-//  delay(10000);    //Send a request every 10 seconds
-   // Halt PICC
-  rfid.PICC_HaltA();
+    rfid.PICC_HaltA();
 
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
+    // Stop encryption on PCD
+    rfid.PCD_StopCrypto1(); 
+  }
+
 }
 
-/**
- * Helper routine to dump a byte array as hex values to Serial. 
- */
 void printHex(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
@@ -136,12 +193,72 @@ String hexToString(byte *buffer, byte bufferSize){
   return hexString;
 }
 
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
 void printDec(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], DEC);
   }
 }
+
+bool readConfigFile() {
+  File f = SPIFFS.open(CONFIG_FILE, "r");
+  
+  if (!f) {
+    Serial.println("Configuration file not found");
+    return false; 
+  } else {
+    
+    size_t size = f.size();
+
+    std::unique_ptr<char[]> buf(new char[size]);
+
+   
+    f.readBytes(buf.get(), size);
+
+    f.close();
+
+    DynamicJsonBuffer jsonBuffer;
+
+    JsonObject& json = jsonBuffer.parseObject(buf.get());
+
+    if (!json.success()) {
+      Serial.println("JSON parseObject() failed");
+      return false;
+    }
+    json.printTo(Serial);
+
+    if (json.containsKey("room")) {
+      room = json["room"];
+      Serial.println(room);
+    }
+    if (json.containsKey("server_ip")) {
+      server_ip = (const char*)json["server_ip"];
+    }
+  }
+  Serial.println("\nConfig file was successfully parsed");
+  return true;
+}
+
+bool writeConfigFile() {
+  Serial.println("Saving config file");
+  DynamicJsonBuffer jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  
+  json["room"] = room;
+  json["server_ip"] = server_ip;
+
+  File f = SPIFFS.open(CONFIG_FILE, "w");
+  if (!f) {
+    Serial.println("Failed to open config file for writing");
+    return false;
+  }
+
+  json.prettyPrintTo(Serial);
+
+  json.printTo(f);
+  f.close();
+
+  Serial.println("\nConfig file was successfully saved");
+  return true;
+}
+  
