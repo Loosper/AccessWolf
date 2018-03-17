7c2501c8702657c390a91de939e281b9c7182a67
Loosper <boian4o1@gmail.com>
2018-03-17 15:59:34 +0200

    Updated manual data entry

--- db_readers/manual_data.py
+++ db_readers/manual_data.py
@@ -42,6 +42,7 @@ with open(sys.argv[1], 'r') as file:
         data = json.loads(file.read())
     except json.JSONDecodeError:
         print('Sorry, incorrect file')
+        sys.exit()
 
     for key in data.keys():
         schema = datatypes[key.lower()]
@@ -53,7 +54,10 @@ with open(sys.argv[1], 'r') as file:
         else:
             new_rows.append(schema(**data[key]))
 
-        session.add_all(new_rows)
-
-
-session.commit()
+        for row in new_rows:
+            try:
+                session.add(row)
+                session.commit()
+            except Exception as e:
+                session.rollback()
+                print(e)
