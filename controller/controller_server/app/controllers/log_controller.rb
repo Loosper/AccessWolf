f8fc4909af4b8b0294e62c7b0b97127d1fc79d62
DanyYanev <yanev89@gmail.com>
2018-03-17 18:19:38 +0200

    working attendances

--- controller/controller_server/app/controllers/log_controller.rb
+++ controller/controller_server/app/controllers/log_controller.rb
@@ -12,23 +12,55 @@ class LogController < ApplicationController
     else
       @currentattendance = CurrentAttendance.where(:student_id => @student.id).first
 
-      if @currentattendance.nil?
+      if @currentattendance.nil? #GETTING IN
         @currentattendance = CurrentAttendance.new
         @currentattendance.student = @student
-        @currentattendance.checkin = Time.now
+        @currentattendance.checkin = DateTime.now.to_s(:time)
+        @currentattendance.room = json["room"]
+
         if @currentattendance.save!
           head 200
         else
           head 401
         end
-      else
+      else #GETTING OUT
         @currentattendance.delete
         if @currentattendance.destroyed?
-          head 200
+
+          @attendance = Attendance.new
+          @attendance.date = DateTime.now.to_s
+          @attendance.student_id = @student.id
+          # @attendance.schedule = find_sch(@currentattendance.checkin, Time.now)
+          # @attendance.attended = attended?(@attendance)
+          find_sch
+          @attendance.schedule_id = @schedule.id
+          @attendance.attended = @attended
+          if @attendance.save!
+            head 200
+          else
+            head 402
+          end
         else
           head 402
         end
       end
+    end
+
+  end
+
+private
+
+  def find_sch
+    @schedule = Schedule.where(:room => @currentattendance.room).where("start_time <= ? AND end_time <= ?", @currentattendance.checkin.to_s(:time), DateTime.now.to_s(:time)).first
+
+    @attended = "true"
+
+    if @schedule.nil?
+      puts "COULDNT FIND SCHEDULE room:#{@currentattendance.room}, #{@currentattendance.checkin.to_s(:time)}, #{DateTime.now.to_s(:time)}"
+      head 332
+    else
+      puts "FOUND SCHEDULE #{@schedule.start_time}, #{@schedule.end_time}"
+      head 200
     end
 
   end
