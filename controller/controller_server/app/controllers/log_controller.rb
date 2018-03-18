dc4696ed54ceeb63236eb18d9a5cd99dd846452f
DanyYanev <yanev89@gmail.com>
2018-03-18 04:08:36 +0200

    added dat damn logic for teacher but the db is broken

--- controller/controller_server/app/controllers/log_controller.rb
+++ controller/controller_server/app/controllers/log_controller.rb
@@ -4,11 +4,28 @@ class LogController < ApplicationController
   def create
     json = JSON.parse request.body.read
 
-    @student = Student.where(:guid => json["uid"]).first;
-    # puts "ROOM IS: #{json["room"]}"
-    # @student = "blabla"
+    @student = Student.where(:guid => json["uid"]).first
     if @student.nil?
-      head 401
+      @teacher = Teacher.where(:guid => json["uid"]).first
+      if !@teacher.nil? #found teacher
+        @pastschedule = PastSchedule.where(:teacher_id => @teacher.id).first
+        if !@pastschedule.nil?
+          @pastschedule.over = true
+          @pastschedule.save
+        else
+          @pastschedule = PastSchedule.new
+          @pastschedule.teacher_id = @teacher.id
+          @pastschedule.occurance_date = DateTime.now.to_s
+          if @pastschedule.save!
+            puts "Wohoo PastSchedule saved"
+            head 200
+          else
+            head 400
+          end
+        end
+
+
+
     else
       @currentattendance = CurrentAttendance.where(:student_id => @student.id).first
 
@@ -32,20 +49,26 @@ class LogController < ApplicationController
           @attendance.student_id = @student.id
           # @attendance.schedule = find_sch(@currentattendance.checkin, Time.now)
           # @attendance.attended = attended?(@attendance)
+          @attended = nil
           find_sch
-          @attendance.schedule_id = @schedule.id
-          @attendance.attended = @attended
-          if @attendance.save!
-            head 200
+          if !@schedule.nil? #if u checked wihtout a schedule there u can fak of
+            @attendance.schedule_id = @schedule.id
+            @attendance.attended = @attended
+            if !@attended.nil?
+              if @attendance.save!
+                head 200
+              else
+                head 402
+              end
+            end
           else
-            head 402
+            head 300 #u nibba checked w/e u want not that it matters
           end
         else
           head 402
         end
       end
     end
-
   end
 
 private
@@ -53,17 +76,14 @@ private
   def find_sch
     @schedule = Schedule.where(:room => @currentattendance.room).where("start_time >= ? AND end_time <= ?", @currentattendance.checkin.to_s(:time), DateTime.now.to_s(:time)).first
 
-    @attended = "true"
-
-    if @schedule.nil?
-      puts "COULDNT FIND SCHEDULE room:#{@currentattendance.room}, #{@currentattendance.checkin.to_s(:time)}, #{DateTime.now.to_s(:time)}"
-      head 332
+    if !@schedule.nil?
+      @attended = "true"
     else
-      puts "FOUND SCHEDULE #{@schedule.start_time}, #{@schedule.end_time}"
-      head 200
+      @schedule = Schedule.where(:room => @currentattendance.room).where("start_time >= ? AND end_time <= ?", (@currentattendance.checkin - 15.minutes).to_s(:time), DateTime.now.to_s(:time)).first
+      if !@schedule.nil?
+        @attended = "late"
+      end
     end
-
   end
-
 
 end
