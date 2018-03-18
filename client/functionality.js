855edc6b3cd0acb8abad70f3b6691dad5886b4e3
DanyYanev <yanev89@gmail.com>
2018-03-18 08:52:08 +0200

    hot fixes

--- client/functionality.js
+++ client/functionality.js
@@ -1,4 +1,3 @@
-//grozen kod
 
 function updateStudentsPresence(classNum, classLetter){
 	var table = document.getElementById('studentsTable');
@@ -7,16 +6,16 @@ function updateStudentsPresence(classNum, classLetter){
 
     //name assigned_class guid id number_in_class
 	alert(res);
-	
+
     var students = JSON.parse(res);
 	//alert(students);
     var i;
     for(i=0; i<students.length; i++){
 	    if(students[i].assigned_class == classNum+classLetter){
-			
+
 			var loc = httpGet("/current_attendances/student/"+students[i].id);
 			var jsonStudent = JSON.parse(res);
-			
+
             addStudentRow(table, students[i].name, students[i].number_in_class, "ENT", jsonStudent.room.toString(), students[i].id);
             var row = table.insertRow(-1);
             var cell = row.insertCell(-1);
@@ -24,7 +23,7 @@ function updateStudentsPresence(classNum, classLetter){
             cell.appendChild(document.createElement("HR"));
         }
     }
-	
+
 	//ime klas nomer vlqzul posl
 }
 
@@ -45,14 +44,123 @@ function updateTeachersPresence(){
 	}
 }
 
+function UpdateSchedule(num, letter){
+	if(num.toString()=="11" && letter=="a"){
+		document.getElementById("a1").innerHTML = "Math";
+		document.getElementById("a2").innerHTML = "Math";
+		document.getElementById("a3").innerHTML = "History";
+		document.getElementById("a4").innerHTML = "History";
+		document.getElementById("a5").innerHTML = "Programming";
+		document.getElementById("a6").innerHTML = "Programming";
+
+		document.getElementById("b1").innerHTML = "English";
+		document.getElementById("b2").innerHTML = "English";
+		document.getElementById("b3").innerHTML = "English";
+		document.getElementById("b4").innerHTML = "Philosophy";
+		document.getElementById("b5").innerHTML = "Philosophy";
+		document.getElementById("b6").innerHTML = "Philosophy";
+
+		document.getElementById("c1").innerHTML = "Programming";
+		document.getElementById("c2").innerHTML = "Programming";
+		document.getElementById("c3").innerHTML = "Literature";
+		document.getElementById("c4").innerHTML = "Literature";
+		document.getElementById("c5").innerHTML = "Web Design";
+		document.getElementById("c6").innerHTML = "Web Design";
+
+		document.getElementById("d1").innerHTML = "Computer Science";
+		document.getElementById("d2").innerHTML = "Computer Science";
+		document.getElementById("d3").innerHTML = "Math";
+		document.getElementById("d4").innerHTML = "Math";
+		document.getElementById("d5").innerHTML = "English";
+		document.getElementById("d6").innerHTML = "English";
+
+		document.getElementById("e1").innerHTML = "Literature";
+		document.getElementById("e2").innerHTML = "Literature";
+		document.getElementById("e3").innerHTML = "Programming";
+		document.getElementById("e4").innerHTML = "Programming";
+		document.getElementById("e5").innerHTML = "Russian";
+		document.getElementById("e6").innerHTML = "Russian";
+	}else if(num.toString()=="11" && letter=="b"){
+		document.getElementById("a1").innerHTML = "English";
+		document.getElementById("a2").innerHTML = "English";
+		document.getElementById("a3").innerHTML = "Literature";
+		document.getElementById("a4").innerHTML = "Literature";
+		document.getElementById("a5").innerHTML = "Math";
+		document.getElementById("a6").innerHTML = "Math";
+
+		document.getElementById("b1").innerHTML = "Web Design";
+		document.getElementById("b2").innerHTML = "Web Design";
+		document.getElementById("b3").innerHTML = "Russian";
+		document.getElementById("b4").innerHTML = "Russian";
+		document.getElementById("b5").innerHTML = "History";
+		document.getElementById("b6").innerHTML = "History";
+
+		document.getElementById("c1").innerHTML = "Math";
+		document.getElementById("c2").innerHTML = "Math";
+		document.getElementById("c3").innerHTML = "Programming";
+		document.getElementById("c4").innerHTML = "Programming";
+		document.getElementById("c5").innerHTML = "Philosophy";
+		document.getElementById("c6").innerHTML = "Philosophy";
+
+		document.getElementById("d1").innerHTML = "Programming";
+		document.getElementById("d2").innerHTML = "Programming";
+		document.getElementById("d3").innerHTML = "English";
+		document.getElementById("d4").innerHTML = "English";
+		document.getElementById("d5").innerHTML = "Math";
+		document.getElementById("d6").innerHTML = "Math";
+
+		document.getElementById("e1").innerHTML = "Programming";
+		document.getElementById("e2").innerHTML = "Programming";
+		document.getElementById("e3").innerHTML = "Literature";
+		document.getElementById("e4").innerHTML = "Literature";
+		document.getElementById("e5").innerHTML = "Russian";
+		document.getElementById("e6").innerHTML = "Russian";
+	}else{
+		document.getElementById("a1").innerHTML = "";
+		document.getElementById("a2").innerHTML = "";
+		document.getElementById("a3").innerHTML = "";
+		document.getElementById("a4").innerHTML = "";
+		document.getElementById("a5").innerHTML = "";
+		document.getElementById("a6").innerHTML = "";
+
+		document.getElementById("b1").innerHTML = "";
+		document.getElementById("b2").innerHTML = "";
+		document.getElementById("b3").innerHTML = "";
+		document.getElementById("b4").innerHTML = "";
+		document.getElementById("b5").innerHTML = "";
+		document.getElementById("b6").innerHTML = "";
+
+		document.getElementById("c1").innerHTML = "";
+		document.getElementById("c2").innerHTML = "";
+		document.getElementById("c3").innerHTML = "";
+		document.getElementById("c4").innerHTML = "";
+		document.getElementById("c5").innerHTML = "";
+		document.getElementById("c6").innerHTML = "";
+
+		document.getElementById("d1").innerHTML = "";
+		document.getElementById("d2").innerHTML = "";
+		document.getElementById("d3").innerHTML = "";
+		document.getElementById("d4").innerHTML = "";
+		document.getElementById("d5").innerHTML = "";
+		document.getElementById("d6").innerHTML = "";
+
+		document.getElementById("e1").innerHTML = "";
+		document.getElementById("e2").innerHTML = "";
+		document.getElementById("e3").innerHTML = "";
+		document.getElementById("e4").innerHTML = "";
+		document.getElementById("e5").innerHTML = "";
+		document.getElementById("e6").innerHTML = "";
+	}
+}
+
 function addTeacherRow(table, name, isIn, lLoc){
 	var row = table.insertRow(-1);
     row.classList.add('teacher-row');
-	
+
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
 	var cell3 = row.insertCell(2);
-	
+
 	cell1.innerHTML = name;
 	cell2.innerHTML = isIn;
 	cell3.innerHTML = lLoc;
@@ -86,7 +194,6 @@ function popUp(uid, name){
 // /attendances/student/(id) <--
 // /schedule
 // /class
-
 // /teachers
 
 // /current_attendances/(student/teacher)/id
