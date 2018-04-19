44a6bb7b585e24cfc45c8a421e619f292c8a7533
Loosper <boian4o1@gmail.com>
2018-04-19 23:50:24 +0300

    mysql

--- interface_server/settings.py
+++ interface_server/settings.py
@@ -1,8 +1,11 @@
 from os.path import abspath, dirname
 
 ROOT = abspath(dirname(dirname(__file__)))
-DB_TYPE = "sqlite+pysqlite"
-DB_PATH = '/' + ROOT + '/database.db'
+# DB_TYPE = "sqlite+pysqlite"
+# DB_PATH = '/' + ROOT + '/database.db'
+
+DB_TYPE = 'mysql+mysqldb'
+DB_PATH = 'root:root@localhost/hacktues'
 PORT = 8080
 HOST = '0.0.0.0'
 
