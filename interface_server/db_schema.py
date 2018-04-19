1eb66a9ab3c8fb8a1ef4b9380142e9fa770a7d7b
Loosper <boian4o1@gmail.com>
2018-04-20 00:11:19 +0300

    not everyone uses virtualenv

--- interface_server/db_schema.py
+++ interface_server/db_schema.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 from sqlalchemy import Column as NullColumn
 
 from sqlalchemy import Integer, String, DateTime, Date, Time,\
--- interface_server/server.py
+++ interface_server/server.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 import tornado.ioloop
 from tornado.web import Application, StaticFileHandler
 
