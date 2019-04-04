200dc8ab6d53374d09e8c592cf0bfeda0e14959f
HeadedSpy <jackroster222@gmail.com>
2019-04-04 21:20:39 +0300

    Adding Bootstarap CSS

--- client/src/EventPage/index.js
+++ client/src/EventPage/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
+
 
 export default function EventPage({ match: { params: { id } } }) {
   return (
-    <div>
-      Events {id}
-    </div>
+    <Container>
+	  <Row>
+		<Col>1 of 2</Col>
+		<Col>2 of 2</Col>
+	  </Row>
+	  <Row>
+		<Col>1 of 3</Col>
+		<Col>2 of 3</Col>
+		<Col>3 of 3</Col>
+	  </Row>
+	</Container>
   )
 }
