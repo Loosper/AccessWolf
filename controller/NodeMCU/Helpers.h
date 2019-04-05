13fcf85ed560d6e0f3c6d0a5146c5f61f960b6a4
Dany Yanev <yanev89@gmail.com>
2019-04-05 19:49:07 +0300

    Rewrote controller code, added separate file for convinience.

---
+++ controller/NodeMCU/Helpers.h
@@ -0,0 +1,258 @@
+#pragma once
+
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson //Using version 5.x
+#include <SoftwareSerial.h>
+
+//flag for saving data
+bool shouldSaveConfig = false;
+String ConfigCardID = "C5-C5-CF-65";
+
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+void SetupWifiManager(char* room_name, char* server_url){
+  //clean FS, for testing
+  //SPIFFS.format();
+  
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    FSInfo fs_info;
+    SPIFFS.info(fs_info);
+    Serial.println(fs_info.totalBytes);
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        Serial.println(String("File size: %d") + String(size));
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(room_name, json["room_name"]);
+          strcpy(server_url, json["server_url"]);
+
+          json.prettyPrintTo(Serial);
+        } else {
+          Serial.println("failed to load json config");
+        }
+        configFile.close();
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter room_name_param("room_name", "Room Name", room_name, 128);
+  WiFiManagerParameter server_url_param("server_url", "Server Url", server_url, 128);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //add all your parameters here
+  wifiManager.addParameter(&room_name_param);
+  wifiManager.addParameter(&server_url_param);
+
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+  
+  //sets timeout until configuration portal gets turned off
+  //useful to make it all retry or go to sleep
+  //in seconds
+  //wifiManager.setTimeout(120);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name and default password
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("AccessWolfPrototype", "administrator")) {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
+  //read updated parameters
+  strcpy(room_name, room_name_param.getValue());
+  strcpy(server_url, server_url_param.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["room_name"] = room_name;
+    json["server_url"] = server_url;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    Serial.println("Got following data, attepting to save to fs:");
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+
+  Serial.println("local ip");
+  Serial.println(WiFi.localIP());
+}
+
+#define SS_PIN 4  //D2
+#define RST_PIN 5 //D1
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+int ReaderTimeOutUntil;
+bool CanRead;
+
+int SetupRFIDReader(MFRC522* reader){
+  Serial.println("Setup Reader");
+  SPI.begin();      // Initiate  SPI bus
+  reader->PCD_Init();   // Initiate MFRC522
+  return 0;
+}
+
+bool ReadRFIDCard(MFRC522* reader, String* card_id){
+  if ( ! reader->PICC_IsNewCardPresent()) 
+  {
+    return false;
+  }
+  // Select one of the cards
+  if ( ! reader->PICC_ReadCardSerial()) 
+  {
+    return false;
+  }
+
+  if(!CanRead){
+    if(ReaderTimeOutUntil < millis()){
+      CanRead = true;
+    }else {
+      return false;
+    }
+  }
+  
+  //Show UID on serial monitor
+  Serial.println();
+  Serial.print(" UID tag :");
+  *card_id = "";
+  byte letter;
+  for (byte i = 0; i < reader->uid.size; i++) 
+  {
+     //Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     //Serial.print(mfrc522.uid.uidByte[i], HEX);
+     card_id->concat(String(reader->uid.uidByte[i] < 0x10 ? " 0" : "-"));
+     card_id->concat(String(reader->uid.uidByte[i], HEX));
+  }
+  card_id->remove(0, 1);
+  card_id->toUpperCase();
+
+  if(*card_id == ConfigCardID){
+    WiFi.disconnect();//erases ssid and password and forces config mode.
+    ESP.reset();
+    delay(1000);
+  }
+  
+  CanRead = false;
+  ReaderTimeOutUntil = millis() + 1500;
+  
+  return true;
+}
+
+#define BUZZER_PIN D8
+
+bool isBuzzing = false;
+int BuzzUntil = 0;
+
+void StartBuzzing(int frequency){
+  isBuzzing = true;
+  tone(BUZZER_PIN, frequency);
+}
+
+void StopBuzzing(){
+  isBuzzing = false;
+  noTone(BUZZER_PIN);
+}
+
+void Buzz(){
+  if(isBuzzing){
+    if(BuzzUntil < millis()){
+      StopBuzzing();
+    }
+  }
+}
+
+void ActivateBuzzer(int code){
+  if(code == 0){
+    if(!isBuzzing){
+      BuzzUntil = millis() + 1000; //Buz for 1 second
+      StartBuzzing(800);
+    }
+  } else if(code == 1){
+    if(!isBuzzing){
+      BuzzUntil = millis() + 300; //Buz for 1 second
+      StartBuzzing(2000);
+    }
+  }
+}
+
+
+String UrlBuilder(char* server_ip, char* room_name, String* card_uid){
+  String URL = "";
+  URL.concat(String("http://"));
+  URL.concat(String(server_ip));
+  URL.concat(String("/api/check_in/"));
+  URL += *card_uid;
+  URL.concat(String("/room/"));
+  URL.concat(String(room_name));
+  Serial.println("URL IS:");
+  Serial.println(URL);
+  return URL;
+}
+
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+
+int SendPostTo(String URL){
+  HTTPClient http;    //Declare object of class HTTPClient
+  http.begin(URL);
+  
+  return http.POST("");
+}
--- controller/NodeMCU/NodeMCU.ino
+++ controller/NodeMCU/NodeMCU.ino
@@ -1,256 +1,48 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
 
-#include <SPI.h>
-#include <MFRC522.h>
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
-#include <ESP8266WebServer.h>
-#include <DNSServer.h>
-#include <WiFiManager.h>
-#include <FS.h>
+//needed for library
 
-#include <ArduinoJson.h>
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+#include "Helpers.h"
 
-#define SS_PIN D8
-#define RST_PIN D2
+//define your default values here, if there are different values in config.json, they are overwritten.
+char room_name[128];
+char server_ip[128];
 
-String server_ip;
-int room;
+// Create MFRC522 instance.
+MFRC522 reader(SS_PIN, RST_PIN); //Pins declared in Helpers.h
+String card_uid = "";
 
-const int TRIGGER_PIN = D3;
-const int BUZZER_PIN = D0;
-const int GREEN_LED_PIN = D4;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
 
-const char* CONFIG_FILE = "/config.json";
-
-bool readConfigFile();
-bool writeConfigFile();
-
-
-MFRC522 rfid(SS_PIN, RST_PIN); 
-
-MFRC522::MIFARE_Key key; 
-
-byte nuidPICC[4];
- 
-void setup () {
-  SPIFFS.begin();
- 
-  Serial.begin(9600);
-   
-  SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
-
-  pinMode(TRIGGER_PIN, INPUT_PULLUP);
-  pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(GREEN_LED_PIN, OUTPUT);
-  
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
-
-  if (!readConfigFile()) {
-    Serial.println("Failed to read configuration file, using default values");
-  } else {
-    WiFi.printDiag(Serial);
-    WiFi.mode(WIFI_STA); 
-    unsigned long startedAt = millis();
-    Serial.print("After waiting ");
-    int connRes = WiFi.waitForConnectResult();
-    float waited = (millis()- startedAt);
-    Serial.print(waited/1000);
-    Serial.print(" secs in setup() connection result is ");
-    Serial.println(connRes);
-  }
-
-  if (WiFi.status()!=WL_CONNECTED){
-    Serial.println("Failed to connect, finishing setup anyway");
-  } else{
-    Serial.print("Local ip: ");
-    Serial.println(WiFi.localIP());
-  }
- 
+  SetupWifiManager(room_name, server_ip);
+  SetupRFIDReader(&reader);
 }
- 
+
 void loop() {
- if ( (digitalRead(TRIGGER_PIN) == LOW)) {
-    Serial.println("Configuration portal requested");
-    
-    char convertedValue[4];
-    WiFiManagerParameter p_room("room", "Room Number", "", 4);
-    
-    WiFiManagerParameter p_server_ip("server_ip", "Server IP", "", 40);
-
-    WiFiManager wifiManager;
-    
-    wifiManager.addParameter(&p_room);
-    
-    wifiManager.addParameter(&p_server_ip);
-
-    if (!wifiManager.startConfigPortal()) {
-      Serial.println("Not connected to WiFi but continuing anyway.");
-    } else {
-     
-      Serial.println("Connected...yeey :)");
-    }
-
-    room = atoi(p_room.getValue());
-    server_ip = String(p_server_ip.getValue());
-    
-    writeConfigFile();
-
-    ESP.reset();
-    
-    delay(5000);
-  }else if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-    
-    if ( ! rfid.PICC_IsNewCardPresent())
-    return;
-    
-    if ( ! rfid.PICC_ReadCardSerial())
-      return;
-      
-    Serial.print(F("PICC type: "));
-    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-    Serial.println(rfid.PICC_GetTypeName(piccType));
-    
-    // Check is the PICC of Classic MIFARE type
-    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-      piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-      Serial.println(F("Your tag is not of type MIFARE Classic."));
-      return;
-    }
-    
-    for (byte i = 0; i < 4; i++) {
-      nuidPICC[i] = rfid.uid.uidByte[i];
-    }
-   
-    Serial.print(F("The NUID tag is:"));
-    String uid = hexToString(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-
-    StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& root = jsonBuffer.createObject();
-
-    root["uid"] = uid;
-    root["room"] = room;
-
-    char jsonBody[100];
-    root.printTo(jsonBody, root.measureLength() + 1);
-    Serial.println(jsonBody);
-    
-    HTTPClient http;
- 
-    http.begin(server_ip);
-    http.addHeader("Content-Type", "application/json");
-    
-    int httpCode = http.POST(String(jsonBody));
-    Serial.print("HTTP request send to: ");
+  
+  if(ReadRFIDCard(&reader, &card_uid)){
+    Serial.println(card_uid);
+    Serial.println(room_name);
     Serial.println(server_ip);
-    if (httpCode == 200) { //Check the returning code
-      Serial.println("JSON Parsed succccsessfully");
-      digitalWrite(GREEN_LED_PIN, HIGH);
-      delay(1000);
-      digitalWrite(GREEN_LED_PIN, LOW);
-    } else if(httpCode == 401){
-      Serial.println("No such database entry");
-      tone(BUZZER_PIN, 800);
-      delay(1000);
-      noTone(BUZZER_PIN);
-    }else{
-      tone(BUZZER_PIN, 800);
-      delay(500);
-      noTone(BUZZER_PIN);
-      delay(200);
-      tone(BUZZER_PIN, 800);
-      delay(500);
-      noTone(BUZZER_PIN);
-      delay(200);
-      tone(BUZZER_PIN, 800);
-      delay(500);
-      noTone(BUZZER_PIN);
-    }
- 
-    http.end();   //Close connection
-    rfid.PICC_HaltA();
 
-    // Stop encryption on PCD
-    rfid.PCD_StopCrypto1(); 
+    UrlBuilder(server_ip, room_name, &card_uid);
+
+    int responceCode = SendPostTo(UrlBuilder(server_ip, room_name, &card_uid));
+
+    if(responceCode == 200){
+      ActivateBuzzer(1);//good
+    }else {
+      ActivateBuzzer(0);//bad
+    }
+    Serial.println(responceCode);
   }
 
+  Buzz();
+
 }
-
-String hexToString(byte *buffer, byte bufferSize){
-  String hexString = String("");
-  for (byte i = 0; i < bufferSize; i++) {
-    hexString = String(hexString + (buffer[i] < 0x10 ? "0" : ""));
-    String tmpString = String(buffer[i], HEX);
-    hexString = String(hexString + tmpString);
-  }
-  Serial.println(hexString);
-  return hexString;
-}
-
-bool readConfigFile() {
-  File f = SPIFFS.open(CONFIG_FILE, "r");
-  
-  if (!f) {
-    Serial.println("Configuration file not found");
-    return false; 
-  } else {
-    
-    size_t size = f.size();
-
-    std::unique_ptr<char[]> buf(new char[size]);
-
-   
-    f.readBytes(buf.get(), size);
-
-    f.close();
-
-    DynamicJsonBuffer jsonBuffer;
-
-    JsonObject& json = jsonBuffer.parseObject(buf.get());
-
-    if (!json.success()) {
-      Serial.println("JSON parseObject() failed");
-      return false;
-    }
-    json.printTo(Serial);
-
-    if (json.containsKey("room")) {
-      room = json["room"];
-      Serial.println(room);
-    }
-    if (json.containsKey("server_ip")) {
-      server_ip = (const char*)json["server_ip"];
-    }
-  }
-  Serial.println("\nConfig file was successfully parsed");
-  return true;
-}
-
-bool writeConfigFile() {
-  Serial.println("Saving config file");
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject& json = jsonBuffer.createObject();
-  
-  json["room"] = room;
-  json["server_ip"] = server_ip;
-
-  File f = SPIFFS.open(CONFIG_FILE, "w");
-  if (!f) {
-    Serial.println("Failed to open config file for writing");
-    return false;
-  }
-
-  json.prettyPrintTo(Serial);
-
-  json.printTo(f);
-  f.close();
-
-  Serial.println("\nConfig file was successfully saved");
-  return true;
-}
-  
