6a4d9e53d97623d4a64605f2cf5218b228066235
DanyYanev <yanev89@gmail.com>
2019-04-23 12:09:23 +0300

    Fixed start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -2,4 +2,4 @@ pip3 install -r requirements.txt
 python3 manage.py makemigrations rest_api
 python3 manage.py migrate
 python3 manage.py loaddata fixtures/initial.json
-python3 manage.py runserver
+python3 manage.py runserver 0:8000
